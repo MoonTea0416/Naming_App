8d633af627c2d476675f6370da31161806e7cb5c
MoonTea0416 <winter0416@hotmail.com>
2022-01-24 01:01:32 -0700

    add dependency and validate to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+      has_many :posts, dependent: :destroy
+      has_many :reactions, dependent: :destroy
+      has_many :comments, dependent: :destroy
+      
       validates :username, presence: true, uniqueness: true, length: { in: 3..15 }
       validates :email, presence: true, uniqueness: true, format: Devise.email_regexp
       validates :fullname, presence: true, length: { in: 3..50 }
