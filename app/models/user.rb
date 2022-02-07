9c62963d1e271c9cb6319d73c00cb4b352bb9fe0
MoonTea0416 <winter0416@hotmail.com>
2022-02-07 00:07:09 -0700

    add user to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
       has_many :posts, dependent: :destroy
       has_many :reactions, dependent: :destroy
       has_many :comments, dependent: :destroy
-      
+
       validates :username, presence: true, uniqueness: true, length: { in: 3..15 }
       validates :email, presence: true, uniqueness: true, format: Devise.email_regexp
       validates :fullname, presence: true, length: { in: 3..50 }
