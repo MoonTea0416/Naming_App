b800089eca6852bdeeab752ea90f9c73197c98ef
MoonTea0416 <winter0416@hotmail.com>
2022-04-05 01:06:15 -0600

    reaction and command

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,5 +8,21 @@ class Post < ApplicationRecord
   validates :user_id, presence: true
   validates :text, presence: true, length: { in: 1..200 }
 
+  def likes
+    self.reactions.filter{ |reaction| reaction.like }.length
+  end
 
+  def dislikes
+    self.reactions.filter{ |reaction| reaction.dislike }.length
+  end
+
+  def liked(user)
+    reaction = reactions.find_by(user_id: user.id)
+    reaction.nil? ? false : reaction.like
+  end
+
+  def disliked(user)
+    reaction = reactions.find_by(user_id: user.id)
+    reaction.nil? ? false : reaction.dislike
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
+  patch '/update_avatar', to: 'users#update_avatar', as: :update_avatar
+  patch '/update_cover', to: 'users#update_cover', as: :update_cover
   resources :users, only: [:index, :show] 
   resources :posts do
     post '/like', to: 'reactions#like', as: :like_action
