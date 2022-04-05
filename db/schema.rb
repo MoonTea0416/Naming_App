f65673f8d626672492d8e78597af8def3ed99b25
MoonTea0416 <winter0416@hotmail.com>
2022-04-05 01:37:31 -0600

    add like and dislike column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_20_092801) do
+ActiveRecord::Schema.define(version: 2022_04_05_073310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,8 @@ ActiveRecord::Schema.define(version: 2022_02_20_092801) do
     t.bigint "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "like"
+    t.boolean "dislike"
     t.index ["post_id"], name: "index_reactions_on_post_id"
     t.index ["user_id"], name: "index_reactions_on_user_id"
   end
